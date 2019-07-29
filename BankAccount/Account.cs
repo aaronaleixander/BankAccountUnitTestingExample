b8b26566d81c3af617684dcbe784863e6aefaf63
Aaron Utterback <aar0nu@outlook.com>
2019-07-29 14:55:54 -0700

    Added unit test project

---
+++ BankAccount/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a customers bank account.
+    /// </summary>
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string Owner { get; set; }
+        public double Balance { get; private set; }
+
+        /// <summary>
+        /// Adds  an amount to the current balance and returns the updated balance.
+        /// </summary>
+        /// <param name="amt">The amount to add</param>
+        /// <returns></returns>
+        public double Deposit(double amt)
+        {
+            if (amt < 0)
+                throw new ArgumentException($"{nameof(amt)} Cannot be negative.");
+
+            Balance += amt;
+            return Balance;
+        }
+
+        /// <summary>
+        /// Withdraw the amount from the current balance.
+        /// </summary>
+        /// <param name="amt">The amount to withdraw</param>
+        /// <returns></returns>
+        public double Withdraw(double amt)
+        {
+            Balance -= amt;
+            return Balance;
+        }
+
+    }
+}
---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void Deposit_PositiveAmount_AddsToBalance()
+        {
+            // AAA = Arrange, Act, Assert
+
+            // Arrange -> Init objects/variables
+            Account checking = new Account();
+            double depositAmt = 10;
+            double expectedBalance = 10;
+
+            // Act -> Call the method under test.
+            checking.Deposit(depositAmt);
+
+            // Assert -> Verification step
+            Assert.AreEqual(expectedBalance, checking.Balance);
+        }
+
+        [TestMethod]
+        public void Deposit_PositiveAmount_ReutrnsUpdatedBalance()
+        {
+            // Arrange
+            Account acc = new Account();
+            double depositAmount = 10.55;
+            double expectedReturn = 10.55;
+
+            // Act
+            double results = acc.Deposit(depositAmount);
+
+            // Assert
+            Assert.AreEqual(expectedReturn, results);
+
+
+        }
+
+        [TestMethod]
+        public void Deposit_NegativeAmount_ThrowsArgumentException()
+        {
+            // Arrange
+            Account acc = new Account();
+            double depositAmount = -1;
+
+            // Act -> Assert vice versa
+            Assert.ThrowsException<ArgumentException>
+                (() => acc.Deposit(depositAmount));
+        }
+    }
+}
